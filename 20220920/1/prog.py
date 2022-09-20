fd499c3048208babedae7e6261ddd0a761a95ed6
Пуртов Богдан <s02200568@vm-mz3-7.prac.cs.msu.ru>
2022-09-20 11:20:58 +0300

    task1

---
+++ 20220920/1/prog.py
@@ -0,0 +1,15 @@
+
+a = eval(input())
+s = "A"
+if a % 25 == 0:
+    if a % 2 == 0:
+        s += " + B - "
+    else:
+        s += " - B + "
+else:
+    s += " - B - "
+if a % 8 == 0:
+    s += "C +"
+else:
+    s += "C -"
+print(s)

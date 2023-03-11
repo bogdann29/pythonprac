a059920b31c2af0217db907c1f83b5b5fa33501c
bogdann29 <Bog_29@mail.ru>
2023-03-11 20:16:49 +0300

    cmd and basic do_command

--- 20230306/1/prog.py
+++ 20230306/1/prog.py
@@ -30,38 +30,56 @@ class cmdLine(cmd.Cmd):
 
 
     def encounter(self, x, y):
-        name, hello = self.monsters[(x, y)]
+        name, hello, hp = self.monsters[(x, y)]
         if name == "jgsbat":
             print(cowsay.cowsay(hello, cowfile=new_monster))
         else:
             print(cowsay.cowsay(hello, cow=name))
 
 
-    def do_up(self):
+    def do_up(self, args):
         self.player_coords[1] = (self.player_coords[1] - 1) % 10
         print(f'Moved to {tuple(self.player_coords)}')
         if tuple(self.player_coords) in self.monsters:
             self.encounter(*self.player_coords)
 
-    def do_down(self):
+    def do_down(self, args):
         self.player_coords[1] = (self.player_coords[1] + 1) % 10
         print(f'Moved to {tuple(self.player_coords)}')
         if tuple(self.player_coords) in self.monsters:
             self.encounter(*self.player_coords)
 
 
-    def do_left(self):
+    def do_left(self, args):
         self.player_coords[0] = (self.player_coords[0] - 1) % 10
         print(f'Moved to {tuple(self.player_coords)}')
         if tuple(self.player_coords) in self.monsters:
             self.encounter(*self.player_coords)
 
     
-    def do_right(self):
+    def do_right(self, args):
         self.player_coords[0] = (self.player_coords[0] + 1) % 10
         print(f'Moved to {tuple(self.player_coords)}')
         if tuple(self.player_coords) in self.monsters:
             self.encounter(*self.player_coords)
+
+
+    def do_addmon(self, args):
+        args = sh.split(args)
+        if len(args) != 8:
+            print("Unknown command")
+            return
+        name = args[0]
+        hello = args[args.index("hello") + 1]
+        hp = int(args[args.index("hp") + 1])
+        x, y = int(args[args.index("coords") + 1]), int(args[args.index("coords") + 2])
+        if name not in cowsay.list_cows():
+            print("Cannot add unknown monster")
+            return
+        print(f'Added monster {name} to {(x, y)} with {hp} hp saying {hello}')
+        if (x, y) in self.monsters:
+            print('Replaced the old monster')
+        self.monsters[(x, y)] = (name, hello, hp)
 
 
     def do_EOF(self, args):
